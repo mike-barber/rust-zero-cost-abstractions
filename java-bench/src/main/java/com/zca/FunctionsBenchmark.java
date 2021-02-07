5b64a1226496711f60f67445d06f792498787a13
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2021-02-07 15:51:26 +0000

    basic java benchmark

--- java-bench/src/main/java/com/zca/FunctionsBenchmark.java
+++ java-bench/src/main/java/com/zca/FunctionsBenchmark.java
@@ -1,27 +1,78 @@
 
 package com.zca;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 
-@State(Scope.Thread)
+@State(Scope.Benchmark)
 public class FunctionsBenchmark {
 
+    static int numVecs = 100;
+    static int vecLength = 20000;
+    private ArrayList<int[]> vectors;
 
+    // thread-local state 
+    @State(Scope.Thread)
+    public static class ThreadState {
+        public static Random rng = new Random();
+    }
+
+    // setup dataset
+    @Setup
+    public void setup() {
+        var rng = new Random();
+        vectors = new ArrayList<int[]>();
+        for (int i=0; i<numVecs; ++i) {
+            var vec = new int[vecLength];
+            for (int j=0; j<vecLength; ++j) {
+                vec[j] = rng.nextInt(10);
+            }
+            vectors.add(vec);
+        }
+    }
+
+    private int[] Sample(Random rng) {
+        var idx = rng.nextInt(vectors.size());
+        return vectors.get(idx);
+    }
 
     @Benchmark
-    @BenchmarkMode(Mode.SampleTime)
-    @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    public int testMethod() {
-        int a = 1;
-        int b = 2;
-        return a + b;
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Fork(value = 1, warmups = 1)
+    public long benchmarkDirect() {
+        long sum = 0;
+        var va = Sample(ThreadState.rng);
+        var vb = Sample(ThreadState.rng);
+        for (int i=0; i<va.length; ++i) {
+            var a = va[i];
+            var b = vb[i];
+            if (a > 2) {
+                sum += a * b;
+            }
+        }
+        return sum;
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Fork(value = 1, warmups = 1)
+    public int benchmarkBaselineSelect() {
+        var va = Sample(ThreadState.rng);
+        var vb = Sample(ThreadState.rng);
+        return va.length + vb.length;
     }
 
 }
