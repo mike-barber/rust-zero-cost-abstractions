e62b57e6293c7c5ac23c2ba23039338f6bd8ca7f
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2021-02-07 12:50:38 +0000

    added checks

--- csharp-bench/FunctionsBench.cs
+++ csharp-bench/FunctionsBench.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Linq;
 using BenchmarkDotNet.Attributes;
+using Xunit;
 
 namespace CsharpBench
 {
@@ -17,11 +19,29 @@ namespace CsharpBench
         public static void SelfTest()
         {
             var self = new FunctionsBench();
-            self.BenchDirect();
-            self.BenchDirectBranchless();
+            self.Direct();
+            self.DirectBranchless();
+            self.DirectUnsafe();
+            self.Iterator();
+            self.IteratorSimpler();
+            self.SelectBaseline();
+
+            // check valid 
+            var va = new[] { 1, 2, 3, 4, 5 };
+            var vb = new[] { 5, 6, 7, 8, 9 };
+            var expected = 3*7 + 4*8 + 5*9;
+
+            Assert.Equal(expected, CalculateDirect(va,vb));
+            Assert.Equal(expected, CalculateDirectBranchless(va,vb));
+            Assert.Equal(expected, CalculateDirectUnsafe(va,vb));
+            Assert.Equal(expected, CalculateIterator(va,vb));
+            Assert.Equal(expected, CalculateIteratorSimpler(va,vb));
+
+            Console.WriteLine("SelfTest checks succeeded");
+            Console.WriteLine();
         }
 
-        public long CalculateDirect(int[] va, int[] vb)
+        public static long CalculateDirect(int[] va, int[] vb)
         {
             long sum = 0;
             for (var i = 0; i < va.Length; ++i)
@@ -36,7 +56,7 @@ namespace CsharpBench
             return sum;
         }
 
-        public long CalculateDirectBranchless(int[] va, int[] vb)
+        public static long CalculateDirectBranchless(int[] va, int[] vb)
         {
             long sum = 0;
             for (var i = 0; i < va.Length; ++i)
@@ -49,7 +69,7 @@ namespace CsharpBench
                 int includeMask = -System.Runtime.CompilerServices.Unsafe.As<bool, int>(ref includeFlag);
 
                 // mutliply apply mask
-                int mult = a*b;
+                int mult = a * b;
                 int gated = includeMask & mult;
 
                 //Console.WriteLine($"{a} {b} gated {gated} include mask {includeMask:x}");
@@ -58,8 +78,61 @@ namespace CsharpBench
             return sum;
         }
 
+        // TODO: create an unrolled or SIMD version 
+        public static long CalculateDirectUnsafe(int[] va, int[] vb)
+        {
+            long sum = 0;
+            unsafe
+            {
+                fixed (int* ptra = va, ptrb = vb)
+                {
+                    var pa = ptra;
+                    var pb = ptrb;
+                    var paEnd = ptra + va.Length;
+
+                    while (pa < paEnd)
+                    {
+                        var a = *pa;
+                        var b = *pb;
+
+                        if (a > 2)
+                        {
+                            sum += a * b;
+                        }
+
+                        pa += 1;
+                        pb += 1;
+                    }
+                }
+            }
+            return sum;
+        }
+
+        public static long CalculateIterator(int[] va, int[] vb)
+        {
+            var res = va.Zip(vb)
+                .Where(pair => pair.First > 2)
+                .Select(pair => (long)(pair.First * pair.Second))
+                .Sum();
+            return res;
+        }
+
+        public static long CalculateIteratorSimpler(int[] va, int[] vb)
+        {
+            var res = va.Zip(vb)
+                .Select(pair =>
+                {
+                    if (pair.First > 2)
+                        return (long)(pair.First * pair.Second);
+                    else
+                        return 0;
+                })
+                .Sum();
+            return res;
+        }
+
         [Benchmark]
-        public long BenchDirect()
+        public long Direct()
         {
             var (va, vb) = testSet.Sample(rng);
             var res = CalculateDirect(va, vb);
@@ -67,11 +140,42 @@ namespace CsharpBench
         }
 
         [Benchmark]
-        public long BenchDirectBranchless()
+        public long DirectBranchless()
         {
             var (va, vb) = testSet.Sample(rng);
             var res = CalculateDirectBranchless(va, vb);
             return res;
+        }
+
+        [Benchmark]
+        public long DirectUnsafe()
+        {
+            var (va, vb) = testSet.Sample(rng);
+            var res = CalculateDirectUnsafe(va, vb);
+            return res;
+        }
+
+        [Benchmark]
+        public long Iterator()
+        {
+            var (va, vb) = testSet.Sample(rng);
+            var res = CalculateIterator(va, vb);
+            return res;
+        }
+
+        [Benchmark]
+        public long IteratorSimpler()
+        {
+            var (va, vb) = testSet.Sample(rng);
+            var res = CalculateIteratorSimpler(va, vb);
+            return res;
+        }
+
+        [Benchmark]
+        public int SelectBaseline()
+        {
+            var (va, vb) = testSet.Sample(rng);
+            return va.Length;
         }
 
 
