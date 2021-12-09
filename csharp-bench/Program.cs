dae7848ee3855c9fce39e4368e8a5483ec9c40f6
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2021-12-09 14:09:58 +0000

    net 6 updates with pgo

--- csharp-bench/Program.cs
+++ csharp-bench/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 
 namespace CsharpBench
@@ -8,7 +10,23 @@ namespace CsharpBench
         static void Main(string[] args)
         {
             FunctionsBench.SelfTest();
-            BenchmarkRunner.Run<FunctionsBench>();
+
+            var job = Job.Default
+                .WithEnvironmentVariables(new EnvironmentVariable[] {
+                    // enable Dynamic Full PGO for .net 6.0
+                    // references:
+                    // - https://devblogs.microsoft.com/dotnet/announcing-net-6/#dynamic-pgo
+                    // - https://devblogs.microsoft.com/dotnet/announcing-net-6/#ryujit
+                    new EnvironmentVariable("DOTNET_TieredPGO", "1"),
+                    new EnvironmentVariable("DOTNET_ReadyToRun", "0"),
+                    new EnvironmentVariable("DOTNET_TC_QuickJitForLoops", "1")
+                })
+                .WithMaxIterationCount(30);
+
+            var config = DefaultConfig.Instance
+                .AddJob(job);
+
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
         }
     }
 }
